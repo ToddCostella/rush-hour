13b97171bb871cbf71390f0cbb68879e1c615337
Todd Costella <ToddCostella@gmail.com>
2025-01-08 16:14:10 -0800

    Cleanup of tests.
    Removed commented out tests that weren't valid due to recent
    refactoring.
    
    Updated coverage tests.

--- tests/test_moves.py
+++ tests/test_moves.py
@@ -146,173 +146,24 @@ def test_horizontal_collision():
     assert new_placement.coverage == car_x_placement.coverage
 
 
-# def test_all_directions_from_square_nine():
-#    g = Game(vehicle_placements=[])
-#    assert g.move_up(9) == 3
-#    assert g.move_down(9) == 15
-#    assert g.move_left(9) == 8
-#    assert g.move_right(9) == 10
-#
-#
-# def test_move_all_directions_from_square_one():
-#    g = Game(vehicle_placements=[])
-#    assert g.move_up(1) == 0
-#    assert g.move_down(1) == 7
-#    assert g.move_left(1) == 0
-#    assert g.move_right(1) == 2
-#
-#
-# def test_move_all_directions_from_square_six():
-#    g = Game(vehicle_placements=[])
-#    assert g.move_up(6) == 0
-#    assert g.move_down(6) == 12
-#    assert g.move_left(6) == 5
-#    assert g.move_right(6) == 0
-#
-#
-# def test_move_all_directions_from_square_thirty_one():
-#    g = Game(vehicle_placements=[])
-#    assert g.move_up(31) == 25
-#    assert g.move_down(31) == 0
-#    assert g.move_left(31) == 0
-#    assert g.move_right(31) == 32
-#
-#
-# def test_move_all_directions_from_square_thirty_six():
-#    g = Game(vehicle_placements=[])
-#    assert g.move_up(36) == 30
-#    assert g.move_down(36) == 0
-#    assert g.move_left(36) == 35
-#    assert g.move_right(36) == 0
-#
-#
-# def test_invalid_move_from_square_one():
-#    g = Game(vehicle_placements=[])
-#    assert g.is_valid_move(Move(from_position=1, direction=Direction.UP)) == False
-#    assert g.is_valid_move(Move(from_position=1, direction=Direction.LEFT)) == False
-#
-#
-# def test_invalid_move_from_square_six():
-#    g = Game(vehicle_placements=[])
-#    assert g.is_valid_move(Move(from_position=6, direction=Direction.UP)) == False
-#    assert g.is_valid_move(Move(from_position=6, direction=Direction.RIGHT)) == False
-#
-#
-# def test_invalid_move_from_square_thirty_one():
-#    g = Game(vehicle_placements=[])
-#    assert g.is_valid_move(Move(from_position=31, direction=Direction.DOWN)) == False
-#    assert g.is_valid_move(Move(from_position=31, direction=Direction.LEFT)) == False
-#
-#
-# def test_invalid_move_from_square_thirty_six():
-#    g = Game(vehicle_placements=[])
-#    assert g.is_valid_move(Move(from_position=36, direction=Direction.DOWN)) == False
-#    assert g.is_valid_move(Move(from_position=36, direction=Direction.RIGHT)) == False
-#
-#
-# def test_invalid_move_from_square_twelve():
-#    g = Game(vehicle_placements=[])
-#    assert g.is_valid_move(Move(from_position=12, direction=Direction.UP)) == True
-#    assert g.is_valid_move(Move(from_position=12, direction=Direction.RIGHT)) == False
-#
-#
-# def test_car_length_two_horizontal_square_1():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=2),
-#        orientation=Orientation.HORIZONTAL,
-#        start_square=1,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [1, 2] == g.calculate_vehicle_placement_squares(vehicle_placement=placement)
-#
-#
-# def test_car_length_three_horizontal_square_1():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=3),
-#        orientation=Orientation.HORIZONTAL,
-#        start_square=1,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [1, 2, 3] == g.calculate_vehicle_placement_squares(
-#        vehicle_placement=placement
-#    )
-#
-#
-# def test_car_length_two_horizontal_square_31():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=2),
-#        orientation=Orientation.HORIZONTAL,
-#        start_square=31,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [31, 32] == g.calculate_vehicle_placement_squares(
-#        vehicle_placement=placement
-#    )
-#
-#
-# def test_car_length_two_vertical_square_1():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=2),
-#        orientation=Orientation.VERTICAL,
-#        start_square=1,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [1, 7] == g.calculate_vehicle_placement_squares(vehicle_placement=placement)
-#
-#
-# def test_car_length_three_vertical_square_1():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=3),
-#        orientation=Orientation.VERTICAL,
-#        start_square=1,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [1, 7, 13] == g.calculate_vehicle_placement_squares(
-#        vehicle_placement=placement
-#    )
-#
-#
-# def test_car_length_two_vertical_square_6():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=2),
-#        orientation=Orientation.VERTICAL,
-#        start_square=6,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [6, 12] == g.calculate_vehicle_placement_squares(vehicle_placement=placement)
-#
-#
-# def test_car_length_three_vertical_square_6():
-#    placement = VehiclePlacement(
-#        car=Car(id=VehicleID("A"), color=Color.GREEN, length=3),
-#        orientation=Orientation.VERTICAL,
-#        start_square=6,
-#    )
-#    g = Game(vehicle_placements=[placement])
-#    assert [6, 12, 18] == g.calculate_vehicle_placement_squares(
-#        vehicle_placement=placement
-#    )
-#
-#
-# def test_two_car_initial_setup():
-#    g = two_car_game()
-#    assert g
-#    assert g.vehicle_placements
-#    assert len(g.vehicle_placements) == 2
-#
-#
-# def test_three_car_coverage():
-#    g = three_car_game()
-#    assert g
-#    assert g.vehicle_placements
-#    assert len(g.vehicle_placements) == 3
-#    covered_squares = sorted(g.calculate_vehicle_placement_coverage_for_all())
-#    assert [3, 4, 6, 12, 14, 15, 18] == covered_squares
-#
-#
-# def test_three_car_other_coverage():
-#    g = three_car_game()
-#    assert g.get_vehicle_placement_for_car(VehicleID("X"))
-#    other_squares = g.calculate_vehicle_placement_coverage_for_others(VehicleID("X"))
-#    my_squares = g.get_my_squares_from_board(VehicleID("X"))
-#    assert set(other_squares).isdisjoint(my_squares)
+def test_three_car_coverage():
+    g = three_car_game()
+    assert g
+    assert g.vehicle_placements
+    assert len(g.vehicle_placements) == 3
+    covered_squares = sorted(g.calculate_vehicle_placement_coverage_for_all())
+    assert [3, 6, 9, 12, 14, 15, 18] == covered_squares
+
+
+def test_other_car_coverage():
+    g = three_car_game()
+    assert g
+    covered_squares = sorted(g.calculate_vehicle_placement_coverage_for_others(car_x))
+    assert [3, 6, 9, 12, 18] == covered_squares
+
+
+def test_red_car_coverage():
+    g = three_car_game()
+    assert g
+    covered_squares = sorted(g.get_my_squares_from_board(car_x))
+    assert [14, 15] == covered_squares
